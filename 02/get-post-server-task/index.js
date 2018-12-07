8dd0bafa0ac10118ddf94071dde1b36d1a89542a
Oleksii Vostrykov <alxdvlpr@gmial.com>
2018-12-07 20:06:36 +0100

    lesson 2.1

--- 02/get-post-server-task/index.js
+++ 02/get-post-server-task/index.js
@@ -9,46 +9,18 @@ server.on('request', (req, res) => {
 
   const pathname = decodeURI(url.parse(req.url).pathname);
 
-    function getFilePath() {
-        let fileName = path.parse(pathname).base;
-        const filePath = path.join(__dirname, 'files', fileName);
-        return filePath;
-    }
-
-    switch(req.method) {
+  switch(req.method) {
   case 'GET':
     if (pathname == '/') {
       const filePath = path.join(__dirname, 'public', 'index.html');
       const fileStream = fs.createReadStream(filePath);
       fileStream.pipe(res);
-    } else if (pathname.length > 1) {
-        const filePath = getFilePath();
-        const fileStream = fs.createReadStream(filePath);
-        fileStream.on('data', chunk => {
-            res.statusCode = 200;
-            res.end(chunk);
-        });
-        fileStream.on('end', () => {
-            res.end();hones
-            
-
-        });
-        fileStream.on('error', () => {
-            res.statusCode = 404;
-            res.end("Not found");
-        });
-    }else {
+    } else {
       res.statusCode = 404;
       res.end("Not found");
     }
 
     break;
-      case 'POST':
-
-          break;
-      case 'DELETE':
-          break;
-
 
   default:
     res.statusCode = 501;
